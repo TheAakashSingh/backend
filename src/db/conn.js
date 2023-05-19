838acb5750dfd5e8f17168552c0307739029debd
Aakash <aakashskilldevelopment@gmail.com>
2023-05-20 00:47:31 +0530

    changes made

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb+srv://aakashskilldevelopment:4X6Ubs3yl6sfei8g@cluster0.qebrfk1.mongodb.net/", {
+mongoose.connect(process.env.MONGOURI ||  "mongodb+srv://aakashskilldevelopment:4X6Ubs3yl6sfei8g@cluster0.qebrfk1.mongodb.net/", {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }).then(() => {
